ec2014ae9e01e8fab6df9c51b073a8bd647b1e3c
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-06-19 21:22:58 +0530

    update the filters component

--- src/app/pages/home/component/filters/filters.component.ts
+++ src/app/pages/home/component/filters/filters.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit,Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,19 +8,40 @@ import { Component, EventEmitter, OnInit,Output } from '@angular/core';
   styles: [
   ]
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCatergory = new EventEmitter<string>();
-
-  categories = ['stores', 'sport'];
-  constructor() { }
+  categoriesSubscription : Subscription | undefined;
+  categories : Array<string> | undefined;
+  constructor(private  storeServices : StoreService) { }
+  
 
   ngOnInit(): void {
+
+    this.categoriesSubscription = this.storeServices.getAllCatergories()
+    .subscribe((response)=>{
+       this.categories = response;
+    });
   }
+
+
+
   onShowCategory(category: string): void {
  this.showCatergory.emit(category)
   }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription){
+      this.categoriesSubscription.unsubscribe();
+    }
+  } 
+
+
+
+ 
 }
+
 function output(): (target: FiltersComponent, propertyKey: "categories") => void {
   throw new Error('Function not implemented.');
 }
+
 
