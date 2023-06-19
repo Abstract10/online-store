fa29013c9e71a72f68ae450a13a08cee5927531f
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-06-19 20:22:43 +0530

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,
-    { enableTracing: true }),BrowserModule],
+   )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product.model';
+const STORE_BASE_URL = 'https://fakestoreapi.com';
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class StoreService {
+
+  constructor(private httpClient : HttpClient) { }
+  // 
+  //
+
+  getAllProducts(
+    limit = '12',
+    sort = 'desc',
+    category?: string
+  ): Observable<Array<Product>> {
+    return this.httpClient.get<Array<Product>>(
+      `${STORE_BASE_URL}/products${
+        category ? '/category/' + category : ''
+      }?sort=${sort}&limit=${limit}`
+    );
+  }
+
+
+   getAllCatergories():Observable<Array<string>>{
+     return this.httpClient.get<Array<string>>(
+      `${STORE_BASE_URL}/Products/categories`
+     )
+   }
+  
+}
