a08281a3e102407b80e1e4e02aeda03fdf62ee48
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-06-19 21:09:32 +0530

    update the cart model structure

--- src/app/model/cart.model.ts
+++ src/app/model/cart.model.ts
@@ -2,9 +2,9 @@ export interface Cart{
     items : Array<CartItems>
 }
 export interface CartItems{
-    product: string ,
-    name: string ,
-    price: number,
-    quantity: number,
+    product: string 
+    name: string 
+    price: number
+    quantity: number
     id:number
 }
