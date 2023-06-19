96cba8843e5ab7982a7c8e2fddee1bf8905d7ed6
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-06-19 21:27:25 +0530

    update the product-header component

--- src/app/pages/home/component/product-header/product-header.component.ts
+++ src/app/pages/home/component/product-header/product-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-product-header',
@@ -7,19 +8,31 @@ import { Component, EventEmitter, OnInit,Output } from '@angular/core';
   ]
 })
 export class ProductHeaderComponent implements OnInit {
-@Output() columnsCountChange = new EventEmitter<number>(); 
+@Output() columnsCountChange = new EventEmitter<number>();
+@Output() itemsCountChange = new EventEmitter<number>(); 
+@Output() sortChange = new EventEmitter<string>();
+
   sort ='desc';
-itemsShowCount =12;
+ itemsShowCount =12;
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
- onsortupdate(newsort:string )  :void{
+
+
+ onsortupdate(newsort:string ):void{
    this.sort =newsort;
+   this.sortChange.emit(newsort);
  }
+
+
  onitemsupdate(count : number):void{
   this.itemsShowCount =count;
+  this.itemsCountChange.emit(count);
  }
 
  onColumnsUpdated(colsNum : number): void{
