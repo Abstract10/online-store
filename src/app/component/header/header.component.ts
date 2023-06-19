933e2e3778af3012e7bf2264a482dc091ca9fe75
Abhijeet <abhijeet@Abhijeet.Abhijeet-Kali>
2023-06-19 21:14:11 +0530

    update the header component

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Cart, CartItems } from 'src/app/model/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
+  private  _cart : Cart ={ items:[] };
+  itemsquantity = 0;
+  
 
-  constructor() { }
+ @Input()
+ get cart():Cart{
+   return this._cart;
+ }
+ set cart(cart:Cart ){
+   this._cart = cart;
+     
+   this.itemsquantity = cart.items
+   .map((item) => item.quantity)
+   .reduce((prev,curr) => prev + curr , 0 ); 
 
-  ngOnInit(): void {
-  }
+ }
+
+ 
+ 
+ constructor( private cartServices: CartService) { }
+ 
+ getTotal(items: Array<CartItems>) : number { 
+  return this.cartServices.getTotal(items);
+}
+onClearCart(){
+   this.cartServices.ClearCart();
+}
 
 }
